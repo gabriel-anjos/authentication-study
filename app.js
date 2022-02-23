7c0d008159c44a2f42d8c174a8ef95ede8491326
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-02-23 17:01:58 -0300

    db conection

--- app.js
+++ app.js
@@ -6,8 +6,21 @@ const jwt = require('jsonwebtoken')
 
 const app = express()
 
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+
+
 app.get('/',(req,res)=>{
     res.status(200).json({msg:'bem vindo a nossa API!'})
 })
 
+//tal caso é ultilizado quando a aplicaçao for full orientada ao banco oq nao tem necessidade.
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.tc2ip.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+.then(()=>{
+    console.log('conectou')
+    
+}).catch((err)=>console.log(err))
+
 app.listen(3000)
+
