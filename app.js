bd42227bfc2a67212a08a63dbe01b70b14a13e2a
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-02-24 17:14:54 -0300

    register user span

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const app = express()
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASSWORD
 
+//Models
+const User = require('./models/User')
+
+app.use(express.json())
+
 
 app.get('/',(req,res)=>{
     res.status(200).json({msg:'bem vindo a nossa API!'})
@@ -19,7 +24,12 @@ app.post('/auth/register', async(req,res)=>{
     //validation
     if(!name){
         return res.status(422).json({msg:"O nome é obrigatório!"})
-
+    }
+    if(!email){
+        return res.status(422).json({msg:"O email é obrigatório!"})
+    }
+    if(!password){
+        return res.status(422).json({msg:"O password é obrigatório!"})
     }
 })
 
