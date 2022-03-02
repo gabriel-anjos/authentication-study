b0cbc35fda894424432e1442279a2626f30aa619
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-03-02 20:17:38 -0300

    checkToken

--- app.js
+++ app.js
@@ -12,11 +12,53 @@ const dbPassword = process.env.DB_PASSWORD;
 //Models
 const User = require("./models/User");
 
+
 app.use(express.json());
+
+//public Route
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "bem vindo a nossa API!" });
 });
+
+//Private Route
+app.get('/user/:id', checkToken, async(req,res)=>{
+    const id = req.params.id
+
+    //check if user exists
+    const user = await User.findById(id,'-password')
+
+    if(!user){
+        return res.status(404).json({msg:'Usuario nao encontrado'})
+    }
+
+
+    res.status(200).json({user})
+
+})
+
+function checkToken(req,res,next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if(!token){
+        return res.status(401).json({msg:'Acesso negado!'})
+    }
+
+    try{
+    
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+
+        next()
+
+    }catch(error){
+        res.status(400).json({msg:'token invalido!'})
+    }
+}
+
+
+
 //register user
 app.post("/auth/register", async (req, res) => {
   const { name, email, password, confirmpassword } = req.body;
